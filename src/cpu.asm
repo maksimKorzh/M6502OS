e38b217bad8ae81a693ab2499c174cdbfb5e7c5c
Maksim Korzh <freesoft.for.people@gmail.com>
2021-09-30 15:58:15 +0300

    added rprint memory debugging routine

--- src/cpu.asm
+++ src/cpu.asm
@@ -17,7 +17,7 @@
 %define          PROCEDURES 0x7e00                          ; global procedures address
 %define        clear_screen 0x0004                          ; clear screen procedure local offset
 %define        print_string 0x0016                          ; print string procedure local offset
-%define        hex_to_ascii 0x0026                          ; hex to ascii procedure local offset
+%define        print_memory 0x0026                          ; hex to ascii procedure local offset
 ;==========================================================================================================
 ;                                                  CPU
 ;==========================================================================================================
@@ -26,14 +26,32 @@ start:                  mov ax, cs                          ; init AX (BOOTSECTO
                         call PROCEDURES:clear_screen        ; init 40 x 25 text video mode
 ;----------------------------------------------------------------------------------------------------------
 
+;mov si, text
+;call PROCEDURES:print_string
+
+mov ax, 0x7c00
+mov ax, es
+mov di, 0x00
+mov ax, 0x1234
+stosw
+mov ax, 0xabcd
+stosw
 
 
 
+
+push ds
+xor ax,ax
+mov ds,ax
+mov si, 0x7c00
+call PROCEDURES:print_memory
+pop ds
 
 
 
 
 jmp $
+text db 'hello',10,13,0
 ;==========================================================================================================
 ;                                               REGISTERS
 ;==========================================================================================================
@@ -42,7 +60,7 @@ register_x              db 0                                ; register B
 register_y              db 0                                ; register C
 register_p              db 0x20                             ; processor flags (NV-BDIZC)
 stack_pointer           db 0xff                             ; stack pointer points to 0x01FF
-program_counter         dw 0xfffc                           ; program machine code starts here
+program_counter         dw 0x0000                           ; address to get where the program code starts
 ;---------------------------------------------------------------------------------------------------------
                         times 512 - ($-$$) db 0x00          ; BIOS bytes padding
 ;=========================================================================================================
--- src/procedures.asm
+++ src/procedures.asm
@@ -33,30 +33,44 @@ clear_screen:           xor ax, ax                          ; reset AX register
                         retf                                ; return from procedure
 ;----------------------------------------------------------------------------------------------------------
 print_string:           cld                                 ; clear direction flag
-print_byte:             lodsb                               ; read next byte from SOURCE INDEX register
+print_string_next:      lodsb                               ; read next byte from SOURCE INDEX register
                         mov ah, 0x0e                        ; BIOS code for teletype output
                         cmp al, 0                           ; match the zero terminating char of the string
-                        je print_return                     ; return if string doesn't contain any chars any more
+                        je print_string_return              ; return if string doesn't contain any chars any more
                         call set_attribute                  ; make character light green
                         int 0x10                            ; assuming ah = 0x0e int 0x10 would print a single char
-                        jmp print_byte                      ; repeat printing char until string is fully printed
-print_return:           retf                                ; return from procedure
+                        jmp print_string_next               ; repeat printing char until string is fully printed
+print_string_return:    retf                                ; return from procedure
 ;----------------------------------------------------------------------------------------------------------
-hex_to_ascii:           cmp al, 10                          ; distinguish between digits and letters
-                        jl digit_to_ascii                   ; convert digit
-                        cmp al, 10                          ; distinguish between digits and letters
-                        jge letter_to_ascii                 ; convert letter  
-digit_to_ascii:         add al, '0'                         ; convert integer to character (digits 0-9)
-                        jmp print_nibble                    ; go and print it
-letter_to_ascii:        add al, 'A' - 10                    ; convert integer to character (letters A-F)
-                        jmp print_nibble                    ; go and print it
-print_nibble:           mov ah, 0x0e                        ; BIOS code to print a char
-                        int 0x10                            ; print character to screen
-                        call set_attribute                  ; make character light green
-                        retf                                ; return from the procedure
-;----------------------------------------------------------------------------------------------------------
-
-;----------------------------------------------------------------------------------------------------------
+print_memory:           cld                                 ; ensure lodsb instruction increments SI register
+                        call print_address                  ; print hex value stored at SI
+                        mov ax, 0x0e3a                      ; colon character
+                        int 0x10                            ; print it
+                        mov ax, 0x0e20                      ; space character
+                        int 0x10                            ; print it
+                        mov ch, 0                           ; CH serves as a byte counter, init it
+print_memory_next:      lodsb                               ; read next byte where SI is pointing to, increment SI register
+                        cmp ch, 8                           ; is there any more bytes left to print
+                        je print_memory_return              ; go to .continue label
+                        mov cl, al                          ; temp store AL to CL
+                        and al, 0xf0                        ; extract 1st nibble => 0xF0 => 1111 0000
+                        shr al, 4                           ; shift 1st nibble 4 bits to the right 1111 0000 => 0000 1111
+                        call print_hex                      ; print 1st nibble
+                        mov al, cl                          ; restore byte value from cl back to al
+                        and al, 0x0f                        ; extract 2nd nibble => 0x0F => 0000 1111
+                        call print_hex                      ; print 2nd nibble
+                        mov ax, 0x0e20                      ; space character
+                        int 0x10                            ; print it
+                        inc ch                              ; increment byte counter
+                        jmp print_memory_next               ; process next byte
+print_memory_return:    mov ax, 0x0e0a                      ; new line character
+                        int 0x10                            ; print it
+                        mov ax, 0x0e0d                      ; carriage return
+                        int 0x10                            ; print it
+                        retf                                ; return to edit loop
+;==========================================================================================================
+;                                              LOCAL PROCEDURES
+;==========================================================================================================
 set_attribute:          push ax                             ; preserve character to print in stack
                         xor ax, ax                          ; reset AX
                         mov ah, 0x09                        ; write character and attribute at cursor position
@@ -65,6 +79,39 @@ set_attribute:          push ax                             ; preserve character
                         pop ax                              ; restore character to print from stack
                         ret                                 ; return to the procedure
 ;----------------------------------------------------------------------------------------------------------
+print_hex:              cmp al, 10                          ; distinguish between digits and letters
+                        jl digit_to_ascii                   ; convert digit
+                        cmp al, 10                          ; distinguish between digits and letters
+                        jge letter_to_ascii                 ; convert letter  
+digit_to_ascii:         add al, '0'                         ; convert integer to character (digits 0-9)
+                        jmp print_nibble                    ; go and print it
+letter_to_ascii:        add al, 'A' - 10                    ; convert integer to character (letters A-F)
+                        jmp print_nibble                    ; go and print it
+print_nibble:           call set_attribute                  ; make character light green
+                        mov ah, 0x0e                        ; BIOS code to print a char
+                        int 0x10                            ; print character to screen
+                        ret                                 ; return from the procedure
+;----------------------------------------------------------------------------------------------------------
+print_address:          mov dx, si                          ; preserve address stored in SI to DX
+                        mov ax, dx                          ; transfer address from DX to AX
+                        and ax, 0xf000                      ; mask 1st digit of address
+                        shr ax, 12                          ; shift right the rest of address
+                        call print_hex                      ; print 1st digit of address
+                        mov ax, dx                          ; transfer address from DX to AX
+                        and ax, 0x0f00                      ; mask 2nd digit of address
+                        shr ax, 8                           ; shift right the rest of address
+                        call print_hex                      ; print 2nd digit of address
+                        mov ax, dx                          ; transfer address from DX to AX
+                        and ax, 0x00f0                      ; mask 3rd digit of address
+                        shr ax, 4                           ; shift right the rest of address
+                        call print_hex                      ; print 3rd digit of address
+                        mov ax, dx                          ; transfer address from DX to AX
+                        and ax, 0x000f                      ; shift right the rest of address
+                        call print_hex                      ; print 4th digit of address
+                        ret                                 ; return from procedure
+;==========================================================================================================
+;                                                  VARIABLES
+;==========================================================================================================
 read_error_message:     db 'Failed to load sector!'         ; read sector error message
 ;---------------------------------------------------------------------------------------------------------
                         times 512 - ($-$$) db 0x00          ; BIOS bytes padding
